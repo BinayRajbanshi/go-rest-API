576008c33faf421d2c54320728a5f1a012fe4591
Binay Rajbanshi <binay@squarebx.com>
2025-03-07 09:37:09 +0545

    graceful server shutdown, better loggers

--- cmd/rest-api/main.go
+++ cmd/rest-api/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	config "github.com/BinayRajbanshi/go-rest-API/internal"
+)
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	slog.Info("Request received: '/'")
+	w.Write([]byte("Welcome to the API,"))
+}
 
 func main() {
-	fmt.Println("Hello world. ")
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+	// setup router
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", handleRoot)
+
+	// setup server
+	server := &http.Server{
+		Addr:    cfg.Address,
+		Handler: mux,
+	}
+
+	slog.Info("Server started: ", slog.String("Address", cfg.Address))
+
+	done := make(chan os.Signal, 1) // The buffer ensures that if multiple signals are received rapidly, none are lost.
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Server is closed")
+		} else if err != nil {
+			slog.Error("Error listening to the server: ")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+	ctx := context.Background()
+	ctx, cancelContext := context.WithTimeout(ctx, 5*time.Second) //ctx.Done() channel is called automatically after 5 seconds. I can also check if context.DeadlineExceeded or context.Canceled
+	defer cancelContext()
+
+	err := server.Shutdown(ctx) //if the provided context expires before the shutdown is complete, Shutdown returns the context's error, otherwise it returns any error returned from closing the [Server]'s underlying Listener(s).
+	if err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown successful")
 }
--- internal/config.go
+++ internal/config.go
@@ -23,6 +23,10 @@ func MustLoad() *Config {
 
 	configPath = os.Getenv("CONFIG_PATH")
 
+	// First tries to get the config path from the CONFIG_PATH environment variable
+	// If not found, looks for a --config command line flag
+	// If neither exists, terminates with a fatal error message
+
 	if configPath == "" {
 		flags := flag.String("config", "", "path to the config file")
 		flag.Parse()
@@ -38,6 +42,7 @@ func MustLoad() *Config {
 		log.Fatalf("Config file does not exist: %s\n", configPath)
 	}
 
+	// Verifies the config file exists, reads it, parses it and finally returns the Config struct pointer
 	var cfg Config
 
 	err := cleanenv.ReadConfig(configPath, &cfg)
@@ -48,7 +53,6 @@ func MustLoad() *Config {
 	return &cfg
 }
 
-// yaml -> take variable from yaml config file
-// env -> take variable from .env file and overwrite the value from yaml file.
-// env-required -> validates the data
-// env-default -> if unable to get value from the files then use this value
+// yaml tags: Define how fields are named in YAML files
+// env tags: Define which environment variables can override config values
+// env-required: Marks fields that must be set
